0dbd514411f1f7798e41e719b1e2d46a7b6f86d2
lihongsheng0228 <lihongsheng0228@163.com>
2018-10-30 23:30:23 +0800

    first commit

---
+++ src/main/java/com/system/controller/DemoController.java
@@ -0,0 +1,23 @@
+package com.system.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class DemoController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home(){
+        return "你好啊！";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(DemoController.class, args);
+    }
+
+}
